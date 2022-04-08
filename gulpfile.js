57e1f19a34b9c6fb21c563c42872751335c6821f
Yegor <shchigel@yahoo.com>
2022-04-08 18:23:21 +0300

    fix gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,18 @@ const gulp = require("gulp");
 const argv = require('yargs').argv;
 const rename = require("gulp-rename");
 const replace = require("gulp-replace");
+const paths = require('./webpack/paths')
 
 const tasks = [
     {
-        src: ['./src/components/_TemplateFunction/_TemplateFunction.js', './src/components/_TemplateFunction/index.js',  './src/components/_TemplateFunction/assets', './src/components/_TemplateFunction/_TemplateFunction.scss'],
-        dest: 'src/components',
+        src: [paths.src + '/components/_TemplateFunction/_TemplateFunction.js', paths.src + '/components/_TemplateFunction/index.js',  paths.src + '/components/_TemplateFunction/assets', paths.src + '/components/_TemplateFunction/_TemplateFunction.scss'],
+        dest: paths.src + '/components',
         base: '_TemplateFunction',
         name: 'func'
     },
     {
-        src: ['./src/components/_TemplateClass/_TemplateClass.js', './src/components/_TemplateClass/index.js',  './src/components/_TemplateClass/assets', './src/components/_TemplateClass/_TemplateClass.scss'],
-        dest: 'src/components',
+        src: [paths.src + '/components/_TemplateClass/_TemplateClass.js', paths.src + '/components/_TemplateClass/index.js',  paths.src + '/components/_TemplateClass/assets', paths.src + '/components/_TemplateClass/_TemplateClass.scss'],
+        dest: paths.src + '/components',
         base: '_TemplateClass',
         name: 'class'
     }
