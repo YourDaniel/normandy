14b533dd6dcad96034e89e098c23fe796c57da25
Daniil Shupikov <dashpkv@gmail.com>
2022-04-09 21:52:46 +0300

    fixed static, templates and root paths, upd package-lock.json

--- __init__.py
+++ __init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from api import MODULES
+import os
+
+
+root_path = os.path.dirname(os.path.dirname(__file__))
 
 
 def create_app():
-    app = Flask('normandy')
+    app = Flask('normandy', root_path=os.path.join(root_path, 'build'),
+                template_folder=os.path.join(root_path, 'build'),
+                static_folder=os.path.join(root_path, 'build')
+                )
     for module in MODULES:
         app.register_blueprint(module.bp)
     return app
--- api/home.py
+++ api/home.py
@@ -1,16 +1,21 @@
 from flask import Blueprint, render_template, send_from_directory
 import os
 
-bp = Blueprint('home', __name__, url_prefix='', root_path=os.path.dirname(os.path.dirname(__file__)))
+root_path = os.path.dirname(os.path.dirname(__file__))
+bp = Blueprint('home', __name__, url_prefix='',
+               root_path=os.path.join(root_path, 'build'),
+               template_folder=os.path.join(root_path, 'build'),
+               static_folder=os.path.join(root_path, 'build'))
 
 
 @bp.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(bp.root_path, 'static'),
+    return send_from_directory(os.path.join(bp.root_path, 'build'),
                                'favicon.ico',
                                mimetype='image/vnd.microsoft.icon')
 
 
 @bp.route('/')
 def home_page():
+    #print(bp.template_folder)
     return render_template('index.html')
